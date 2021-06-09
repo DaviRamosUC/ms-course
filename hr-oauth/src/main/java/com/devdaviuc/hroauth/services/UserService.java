0a3915d4e74deea8d1f9206ebd7a55901b4f93fc
Davi <davi.lima@ucsal.edu.br>
2021-06-09 15:37:32 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/devdaviuc/hroauth/services/UserService.java
@@ -0,0 +1,28 @@
+package com.devdaviuc.hroauth.services;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devdaviuc.hroauth.entities.User;
+import com.devdaviuc.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+	
+	Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user =userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			logger.error("Email not found " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + email);
+		return user;
+	}
+	
+}
