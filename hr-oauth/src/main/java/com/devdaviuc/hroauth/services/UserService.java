045aa38e494e02dd6253524717dd7445ad927451
Davi <davi.lima@ucsal.edu.br>
2021-06-09 17:13:49 -0300

    Login, jwt token

--- hr-oauth/src/main/java/com/devdaviuc/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devdaviuc/hroauth/services/UserService.java
@@ -2,13 +2,16 @@ package com.devdaviuc.hroauth.services;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devdaviuc.hroauth.entities.User;
 import com.devdaviuc.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 
@@ -23,6 +26,18 @@ public class UserService {
 		}
 		logger.info("Email found: " + email);
 		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user =userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
+		return user;
+		
 	}
 	
 }
